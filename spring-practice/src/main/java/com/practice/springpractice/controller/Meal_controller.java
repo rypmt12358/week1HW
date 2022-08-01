f0792975621b605b96cb18c5633f1dfe67a68096
rypmt12358 <66083765+rypmt12358@users.noreply.github.com>
2022-08-01 12:59:56 +0800

    Add files via upload

---
+++ spring-practice/src/main/java/com/practice/springpractice/controller/Meal_controller.java
@@ -0,0 +1,30 @@
+package com.practice.springpractice.controller;
+
+import com.practice.springpractice.model.Meal;
+import com.practice.springpractice.model.Order;
+import com.practice.springpractice.service.Meal_Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+
+public class Meal_controller {
+    @Autowired
+    Meal_Service meal_service;
+
+
+    @GetMapping("/meal")
+    public String getMealByName(@RequestParam String name, Model model){
+        Meal meal=this.meal_service.getMealByName(name);
+        model.addAttribute("meal",meal);
+        return "meal";
+    }
+
+
+
+}
---
+++ spring-practice/src/main/java/com/practice/springpractice/model/Order.java
@@ -0,0 +1,51 @@
+package com.practice.springpractice.model;
+
+import java.util.List;
+
+public class Order {
+    private int totalPrice;
+    private String waiter;
+    private List<Meal> meals;
+    private String seq;
+
+    public String getSeq() {
+        return seq;
+    }
+
+    public void setSeq(String seq) {
+        this.seq = seq;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getWaiter() {
+        return waiter;
+    }
+
+    public void setWaiter(String waiter) {
+        this.waiter = waiter;
+    }
+
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
+    }
+
+    public Order(String seq, int totalPrice, String waiter, List<Meal> meals) {
+        this.seq = seq;
+        this.totalPrice = totalPrice;
+        this.waiter = waiter;
+        this.meals = meals;
+    }
+
+
+}
